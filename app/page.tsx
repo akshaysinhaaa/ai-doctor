4af95212e385d375dc9cfedfd765143a03c5f763
Akshay <akshay@Akshays-MacBook-Air-2.local>
2025-02-05 19:12:10 +0530

    initialized

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,14 @@ type Props = {}
 
 const HomeComponent = (props: Props) => {
   return (
-    <div>HomeComponent</div>
+    <div className='grid h-screen -w-full'>
+      <div className='flex flex-col'>
+        <header className='sticky top-0 z-10 h-[57px] bg-background flex items-center gap-1 border-b px-4'>
+          <h1 className='text-xl font-semibold text-[#D90013]'>AI Doctor</h1>
+          <div className='w-full flex flex-row justify-end gap-2'></div>
+        </header>
+      </div>
+    </div>
   )
 }
 
