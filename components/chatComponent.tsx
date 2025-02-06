0eaa67894ba2a297d548606837fa7d9a5f907057
Akshay Sinha <alt.akshay.sinha@gmail.com>
2025-02-07 00:50:21 +0530

    added badge

--- components/chatComponent.tsx
+++ components/chatComponent.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Badge } from "@/components/ui/badge"
 
 type Props = {
     reportData: string
@@ -6,7 +7,9 @@ type Props = {
 
 const ChatComponent = ({ reportData }: Props) => {
   return (
-    <div className='h-full bg-muted/50 relative flex flex-col min-h-[50vh] rounded-xl p-4 gap-4'>ChatComponent</div>
+    <div className='h-full bg-muted/50 relative flex flex-col min-h-[50vh] rounded-xl p-4 gap-4'>
+        <Badge className='absolute right-3 top-1.5' variant={'outline'}>{reportData ? 'Report Added' : 'No Report Added'}</Badge>
+    </div>
   )
 }
 
