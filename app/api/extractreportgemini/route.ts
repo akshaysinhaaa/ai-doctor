deb5ed003fdaef75af6b85c1933abb447f336ee8
Akshay Sinha <alt.akshay.sinha@gmail.com>
2025-02-06 23:00:59 +0530

    working on google gemini api

--- app/api/extractreportgemini/route.ts
+++ app/api/extractreportgemini/route.ts
@@ -1,4 +1,28 @@
+import { GoogleGenerativeAI } from "@google/generative-ai"
+const genAi = new GoogleGenerativeAI(process.env.GEMINI_API_KEY!)
+const model = genAi.getGenerativeModel({
+    model: 'gemini-1.5-pro'
+})
+
+const prompt = `Attached is an image of a clinical report. 
+Go over the the clinical report and identify biomarkers that show slight or large abnormalities. Then summarize in 100 words. You may increase the word limit if the report has multiple pages. Do not output patient name, date etc. Make sure to include numerical values and key details from the report, including report title.
+## Summary: `;
+
 export async function POST(req:Request, res:Response){
     const {base64} = await req.json();
+    const filePart = fileToGenerativePart(base64);
+    const generatedContent = await model.generateContent([prompt, filePart]);
+    console.log(generatedContent);
+}
 
+function fileToGenerativePart(imageData: string) {
+    return {
+        inlineData: {
+            data: imageData.split(",")[1],
+            mimeType: imageData.substring(
+                imageData.indexOf(":") + 1,
+                imageData.lastIndexOf(";")
+            ),
+        },
+    }
 }
