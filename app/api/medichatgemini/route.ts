b6dd4319f4bda14ba030c6ade3a0a1ceb81b1996
Akshay Sinha <e22cseu0385@bennett.edu.in>
2025-04-02 23:10:17 +0530

    updated utils.ts

--- app/api/medichatgemini/route.ts
+++ app/api/medichatgemini/route.ts
@@ -1,6 +1,18 @@
+import { Message } from "ai/react";
+import { Pinecone } from '@pinecone-database/pinecone';
+import { queryPineconeVectorStore } from "@/utils";
+
+const pc = new Pinecone({ apiKey: process.env.PINECONE_API_KEY! });
+
 export async function POST(req:Request, res:Response){
     const reqBody = await req.json();
     console.log(reqBody);
+    const messages: Message[] = reqBody.messages;
+    const userQuestion = messages[messages.length - 1].content;
+    const reportData = reqBody.data.reportData;
+
+    const searchQuery = `Patient medical report says: \n${reportData} \n\n ${userQuestion}`;
+    const retrievals = await queryPineconeVectorStore(pc, 'medic', 'testspace', searchQuery)
 
     return new Response("Success", {status: 200});
 }
--- components/messageBox.tsx
+++ components/messageBox.tsx
@@ -12,7 +12,7 @@ const MessageBox = ({role, content}: Props) => {
             <CardContent>
                 {content}
             </CardContent>
-            {role != 'user' && <CardFooter className="border-t bg-muted/50 px-6 py-3 text-xs text-muted-foreground">
+            {role !== 'user' && <CardFooter className="border-t bg-muted/50 px-6 py-3 text-xs text-muted-foreground">
                 Disclaimer: The medical advice and recommendations provided by this
                 application are for informational purposes only and should not
                 replace professional medical diagnosis, treatment, or advice.
